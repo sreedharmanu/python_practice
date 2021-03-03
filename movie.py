dafdfcb27e86c14fc4e8dc143f1d2b00489d8bc5
sreedharmanu <sreedharkumarpsk1994@gmail.com>
2021-03-03 13:40:56 +0530

    movies

---
+++ movie.py
@@ -0,0 +1,14 @@
+age=int(input("what is your age"))
+if age > 18:
+    print("english movie")
+elif age <18 and age>11:
+    print("hindi movies")
+else:
+    print("telugu movie")
+	
+    
+    
+    
+    
+    
+    
